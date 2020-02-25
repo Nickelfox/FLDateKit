fd30c5b80fade7ad3bf0cee2995b87d02e9292e6
Abhishek Thapliyal <abhishekthapliyal7579@gmail.com>
2020-02-25 13:23:36 +0530

    trivial commit

---
+++ Source/Date+String.swift
@@ -0,0 +1,17 @@
+//
+//  Date+String.swift
+//  FLDateKit
+//
+//  Created by Abhishek Thapliyal on 25/02/20.
+//  Copyright © 2020 Abhishek Thapliyal. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    
+    var dummyMessage: String {
+        return "THIS IS POD DEMO"
+    }
+    
+}
